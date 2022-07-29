e321560a64a3adf9e8b4d6da575dabc778c35c99
MiguelRosado-WCCI <M.rosado1725@gmail.com>
2022-07-29 14:24:26 -0400

    Int

--- Backend/album-collections-blackshirts/album-collections-blackshirts/MusicContext.cs
+++ Backend/album-collections-blackshirts/album-collections-blackshirts/MusicContext.cs
@@ -17,14 +17,23 @@ namespace album_collections_blackshirts
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+         {
             modelBuilder.Entity<Artist>().HasData(
                 new Artist() { Id = 1, Name = "Kanye West", Age = 45, Hometown = "Chicago, IL", RecordLabel = "GOOD Music" },
-                new Artist() { Id = 2, Name = "System of a Down", Age = 28, Hometown = "Glendale, CA", RecordLabel = "American Recordings"  }
+                new Artist() { Id = 2, Name = "System of a Down", Age = 28, Hometown = "Glendale, CA", RecordLabel = "American Recordings"  },
+                new Artist() { Id = 3, Name = "Notorious B.I.G", Age = 24, Hometown = "Brooklyn, NY", RecordLabel = "Bad Boy Records"},
+                new Artist() { Id = 4, Name = "Queen", Age = 52, Hometown = "London, ENG", RecordLabel = "EMI Records"}
+
                 );
             modelBuilder.Entity<Album>().HasData(
-                new Album() { Id = 1, Title = "Donda 2", RecordLabel = "GOOD Music", ArtistId = 1 },
-                new Album() { Id = 2, Title = "Hypnotize", RecordLabel = "American Recordings", ArtistId = 2 }
+                new Album() { Id = 1, Title = "The College Dropout", RecordLabel = "Roc-A-Fella Records", ArtistId = 1 },
+                new Album() { Id = 2, Title = "Donda 2", RecordLabel = "GOOD Music", ArtistId = 1 },
+                new Album() { Id = 3, Title = "Toxicity", RecordLabel = "American Recordings", ArtistId = 2 },
+                new Album() { Id = 4, Title = "Hypnotize", RecordLabel = "American Recordings", ArtistId = 2 },
+                new Album() { Id = 5, Title = "Ready To Die", RecordLabel = "Bad Boy Records", ArtistId = 3 },
+                new Album() { Id = 6, Title = "Life After Death", RecordLabel = "Bad Boy Records", ArtistId = 3},
+                new Album() { Id = 7, Title = "Queen", RecordLabel = "EMI Records", ArtistId = 4 },
+                new Album() { Id = 8, Title = "Made In Heaven", RecordLabel = "EMI Records", ArtistId = 4}
                 );
            
             base.OnModelCreating(modelBuilder);
